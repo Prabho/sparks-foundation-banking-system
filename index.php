dc0b23a1ad38826b5d665b52d817e8a250bfea3e
Prabho <71812263+Prabho@users.noreply.github.com>
2021-12-21 17:53:02 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php include('nav.php') ?>
+
+
+  <h1>BASIC BANKING SYSTEM</h1>
+
+
+  <div class="bg">
+    <div class="center">
+      <ul>
+        <li class="operations"><a href="transfermoney.php"><button class="btn" id="green">View All Users</button></a></li>
+        <li class="operations"><a href="transfermoney.php"><button class="btn" id="green">Transfer Money</button></a></li>
+        <li class="operations"><a href="transactionhistory.php"><button class="btn" id="green">Transfer History</button></a></li>
+      </ul>
+    </div>
+  </div>
+
+  <?php include('footer.php') ?>
+  
+</body>
+</html>
